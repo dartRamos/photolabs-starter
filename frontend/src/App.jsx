aa92c95619f33015a9b046a1a56cca418979fd60
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-03-05 12:37:46 -0500

    Built components and styling for TopNavigationBar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+import TopNavigation from './components/TopNavigationBar';
 import './App.scss';
 
 const App = () => (
   <div className="App">
-    <TopicList />
+    <TopNavigation />
     <PhotoList />
   </div>
 )
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,9 +1,15 @@
 import '../styles/TopNavigationBar.scss'
+import FavIcon from './FavIcon';
+import TopicList from './TopicList';
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
+
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList />
+      <FavIcon selected={props.selected} />
     </div>
   )
 };
