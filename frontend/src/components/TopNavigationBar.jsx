a2967eb8e280e7e5bf4d34a4a325e3036796033e
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-03-05 13:02:19 -0500

    Corrected added FavBadge to Navigation instead of FavIcon

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,17 +1,15 @@
-import '../styles/TopNavigationBar.scss'
-import FavIcon from './FavIcon';
+import '../styles/TopNavigationBar.scss';
+import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
 const TopNavigation = (props) => {
-
-
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList />
-      <FavIcon selected={props.selected} />
+      <FavBadge isFavPhotoExist={props.isFavPhotoExist} selected={props.selected} />
     </div>
-  )
+  );
 };
 
 export default TopNavigation;
